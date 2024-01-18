05dc6be399ec95c20e87874acecc40c4a0f58dfd
Haaki <haakon@aarstein.net>
2024-01-18 18:23:06 +0100

    Added a basic enemy script
    just a red square that follows the player for now

---
+++ Script/enemy.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@onready var player = %SquareBoi
+
+var health
+
+func _physics_process(_delta):
+
+	var direction = global_position.direction_to(player.global_position)
+
+	var target_velocity = direction * 100
+
+	velocity += (target_velocity - velocity) * 0.1
+
+	move_and_slide()
