d60effb8c3a05ee7e1576ea6b752dcd113b996d7
MechaMarty <martine.gjermundsen.raestad@outlook.com>
2024-01-21 01:44:18 +0100

    making direction arrow for Player

--- Script/square_boi.gd
+++ Script/square_boi.gd
@@ -6,3 +6,10 @@ func _physics_process(_delta):
 	velocity = direction * 600
 	move_and_slide()
 	
+func _process(_delta):
+	if Input.is_action_pressed("MoveLeft")
+		_animation_player.play("MovingLeft")
+	else:
+		_animation_player.play("PlayerIdle")
+		
+		
