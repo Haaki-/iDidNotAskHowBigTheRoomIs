ef1f8c6f9016cc3fe31d96aee69f16ea6cd152a7
Haaki <haakon@aarstein.net>
2024-01-21 03:23:48 +0100

    Fixed player direction animation logic

--- Script/square_boi.gd
+++ Script/square_boi.gd
@@ -11,11 +11,11 @@ func _physics_process(_delta):
 func _process(_delta):
 	if Input.is_action_pressed("MoveLeft"):
 		_animation_player.play("MovingLeft")
-	if Input.is_action_pressed("MoveRight"):
+	elif Input.is_action_pressed("MoveRight"):
 		_animation_player.play("MovingRight")
-	if Input.is_action_pressed("MoveUp"):
+	elif Input.is_action_pressed("MoveUp"):
 		_animation_player.play("MovingUp")
-	if Input.is_action_pressed("MoveDown"):
+	elif Input.is_action_pressed("MoveDown"):
 		_animation_player.play("MovingDown")
 	else:
 		_animation_player.play("PlayerIdle")
