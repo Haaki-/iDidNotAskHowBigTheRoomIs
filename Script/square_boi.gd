5b000eebeaf0c6b3fb967d4ba27f1d4e4a82d10f
MechaMarty <martine.gjermundsen.raestad@outlook.com>
2024-01-21 01:52:10 +0100

    movement arrow part 2

--- Script/square_boi.gd
+++ Script/square_boi.gd
@@ -6,9 +6,17 @@ func _physics_process(_delta):
 	velocity = direction * 600
 	move_and_slide()
 	
+@onready var _animation_player = $AnimationPlayer
+	
 func _process(_delta):
-	if Input.is_action_pressed("MoveLeft")
+	if Input.is_action_pressed("MoveLeft"):
 		_animation_player.play("MovingLeft")
+	if Input.is_action_pressed("MoveRight"):
+		_animation_player.play("MovingRight")
+	if Input.is_action_pressed("MoveUp"):
+		_animation_player.play("MovingUp")
+	if Input.is_action_pressed("MoveDown"):
+		_animation_player.play("MovingDown")
 	else:
 		_animation_player.play("PlayerIdle")
 		
