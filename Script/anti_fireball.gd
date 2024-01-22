4b39721f5934c7bd8fbb11b856e2b23c48f3ca7d
MechaMarty <martine.gjermundsen.raestad@outlook.com>
2024-01-22 21:01:12 +0100

    Should delete off-screen bullets

--- Script/anti_fireball.gd
+++ Script/anti_fireball.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-var velocityBullet = Vector2(0, 1)
+var velocityBullet = Vector2(0, 0)
 var speed = 300
 
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(velocityBullet.normalized() * delta * speed)
-	if collision_info:
-		velocity = velocity.bounce(collision_info.get_normal())
+	var collision = move_and_collide(velocityBullet.normalized() * delta * speed)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
 	
-	
+func _on_VisibilityNotifier2D_screen_exited():
+	# Deletes the bullet when it exits the screen.
+	queue_free()
 	
