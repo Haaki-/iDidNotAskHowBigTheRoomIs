ed2bbe12d229be6b1303072c5ea4380166c2f1f4
Haaki <haakon@aarstein.net>
2024-01-27 00:39:19 +0100

    Made the fire wiggle more, and slightly chase the player

--- Script/flame.gd
+++ Script/flame.gd
@@ -2,25 +2,32 @@ extends CharacterBody2D
 
 @onready var target = get_node("/root/GameRoot/SquareBoi")
 
-var health
+var health = 1
 
-# @onready var _animation_player = $AnimationPlayer
+const CHASE_SPEED = 20
+const WIGGLE_SPEED = 300
 
-# func _process(_delta):
-# 	if velocity.is_zero_approx():
-# 		# _animation_player.play("idle")
-# 	if velocity.is_normalized():
-# 		# _animation_player.play("idle")
+func _ready():
+	%BurnoutTimer.start( randi() % 10 )
+
 
 func _physics_process(_delta):
 
-	var direction = Vector2( randf() - 0.5, randf() - 0.5 )
+	var direction = global_position.direction_to(target.global_position) * CHASE_SPEED
+	direction += Vector2( randf() - 0.5, randf() - 0.5 ) * WIGGLE_SPEED
 
-	var target_velocity = direction * 200
+	var target_velocity = direction
 
 	velocity += (target_velocity - velocity) * 0.1
 
 	move_and_slide()
 
+func take_damage(damage):
+	health -= damage
+	
+	if health <= 0:
+		die()
+
 func die():
 	queue_free()
+
