344f99b94740333ccc6c4e38b3f269c92ef52813
Haaki <haakon@aarstein.net>
2024-01-27 00:25:51 +0100

    Enemies can now be defeated

--- Script/enemy.gd
+++ Script/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var target = get_node("/root/GameRoot/SquareBoi")
 
-var health
+var health = 3
 
 # @onready var _animation_player = $AnimationPlayer
 
@@ -21,6 +21,12 @@ func _physics_process(_delta):
 	velocity += (target_velocity - velocity) * 0.1
 
 	move_and_slide()
+
+func take_damage(damage):
+	health -= damage
+	
+	if health <= 0:
+		die()
 
 func die():
 	queue_free()
--- Script/fire_spawner.gd
+++ Script/fire_spawner.gd
@@ -3,10 +3,11 @@ extends Node2D
 const ENEMY = preload("res://Scene/Enemies/enemy_flame.tscn")
 
 func spawn():
-	var new_enemy = ENEMY.instantiate()
+	var new_flame = ENEMY.instantiate()
+	new_flame.health = 1
 	%SpawnPoint.progress_ratio = randf()
-	new_enemy.global_position = %SpawnPoint.global_position
-	get_node("/root/GameRoot/").add_child(new_enemy)
+	new_flame.global_position = %SpawnPoint.global_position
+	get_node("/root/GameRoot/").add_child(new_flame)
 
 func _on_timer_timeout():
 	spawn()
